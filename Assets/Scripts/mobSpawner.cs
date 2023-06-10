ca056a5ac1be72cb950861125d81e7926cf84f69
LooserRIP <korenandkoren@gmail.com>
2023-06-10 19:27:54 +0300

    added thuings

---
+++ Assets/Scripts/mobSpawner.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class mobSpawner : MonoBehaviour
+{
+    public int minimumMobs;
+    public int maximumMobs;
+    public Vector2 spread;
+    public int[] mobTypes;
+    public float weight;
+    public float delayMinimum;
+    public float delayMaximum;
+    public float range = 999f;
+    public Sprite activatedSprite;
+    public SpriteRenderer sr;
+    
+
+
+}
